e9b71b48bb61adc8efa436f0bd5b6b1a6ba2da2e
Iamneo <techadmin@iamneo.ai>
2023-09-14 09:34:14 +0000

    Backup Commit

--- scripting5.sh
+++ scripting5.sh
@@ -1,7 +1,8 @@
 read -p "Enter the first number: " num1
 read -p "Enter the first number: " num2
-total=$(($num1 + $num2))
-if [[ $total > 70 ]]; then
+total=$(( $num1 + $num2 ))
+echo $total
+if [ $total -gt 70 ]; then
 echo "Pass"
 else
 echo "Fail"

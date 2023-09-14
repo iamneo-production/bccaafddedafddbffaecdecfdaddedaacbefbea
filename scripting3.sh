f225f598f89b67563f436221d71408aee06c9753
Iamneo <techadmin@iamneo.ai>
2023-09-14 08:44:14 +0000

    Backup Commit

--- scripting3.sh
+++ scripting3.sh
@@ -5,5 +5,5 @@ mkdir "$home/$directory_name" chmod 444"$home/$directory_name"
 echo "Created directory: $directory_name"
 #create the new file with read-only permissions for all
 touch "$home/$directory_name/$file_name"
-chmod 444 "$HOME/$directory_name/$file_name"
+chmod 444 "$home/$directory_name/$file_name"
 echo "Created file: $file_name"
